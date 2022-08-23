e7827de69f7c4373eda466d2a2b4f3011f3a899d
Gabriel Victor <111542678+gavictorr@users.noreply.github.com>
2022-08-22 22:38:16 -0300

    Seção Home
    Home ainda não finalizada, somente feito a navbar com flexbox e adaptação para dispositivos mobile.

---
+++ HOME/index.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="PT-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=<device-width>, initial-scale=1.0">
+    <title>UPA LFN</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <?php
+        include ("script.php");
+    ?>
+    <div id="inicio">
+        <header>   
+            <nav>
+                <a class="logo" href="/">UPA Luis Felipe Naumann</a>
+                <div class="mobile-menu">
+                    <div class="line1"></div>
+                    <div class="line2"></div>
+                    <div class="line3"></div>
+                </div>
+                <ul class="nav-list">
+                    <li><a href="/">Formulário</a></li>
+                    <li><a href="/">Triagem</a></li>
+                    <li><a href="/">Paciente</a></li>
+                    <li><a href="/">Médicos</a></li>
+                    <li><a href="/">Seção Avaliativa</a></li>
+                </ul>
+            </nav>
+        </header>
+        <main></main>
+        <script src="mobile-navbar.js"></script>
+    </div>
+    <div id="meio">
+        <div id="informações_upa">
+
+        </div>
+        <div id="atendimentos">
+
+        </div>
+        <div id="medicos">
+
+        </div>
+        <div id="enfermeiros">
+
+        </div>
+        <div id="informações_site">
+
+        </div>
+        <div>
+
+        </div>
+    </div>
+</body>
+</html>
